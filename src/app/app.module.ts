0f9e5464610d73a03322efa5d531ccb768a3b265
Vitoria <vitoria10_@live.com>
2020-04-25 14:09:22 -0300

    aplicando configurações em 'app.module.ts'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TodayComponent } from './today/today.component';
 
+import { HttpClientModule } from '@angular/common/http'; //necessario para consumir a api
+
 @NgModule({
   declarations: [
     AppComponent,
     TodayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
