7cc33a9ed42a91789f42ac02fcf6c9d52cc578a3
Vitoria <vitoria10_@live.com>
2020-04-26 00:55:36 -0300

    alguns ajustes

--- src/app/wheather.service.ts
+++ src/app/wheather.service.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class WheatherService {
 
-  url = 'https://openweatherapp.org/data/2.5/';
+  url = 'https://api.openweatherapp.org/data/2.5/weather';
   apiKey = 'dbf7ed89db2eda547f062a62a0bb2a31';
 
   constructor(private http: HttpClient) { }
@@ -19,19 +19,19 @@ export class WheatherService {
       .set('units', 'metric')
       .set('appid', this.apiKey);
 
-  return this.http.get(this.url, { params });
+      return this.http.get(this.url, { params });
 
-  //preciso entender melhor esta parte, este método.
+  // preciso entender melhor esta parte, este método.
 }
 
   getWeatherDataByCityName(city: string){
-    //definir os parametros da url
+    // definir os parametros da url
     const params = new HttpParams()
       .set('q', city)
       .set('units','metric')
       .set('appid', this.apiKey);
 
-      return this.http.get(this.url, { params });
+    return this.http.get(this.url, { params });
   }
 
 
